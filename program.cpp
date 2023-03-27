2f1392069ab2c5ea86cb901a7207f43c5186c870
Darya <daryaeremeeva2013@mail.ru>
2023-03-27 21:31:57 +0300

    original version of program

---
+++ program.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, c, choice;
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
+    cout << "Enter your choice:\n1. Addition\n2. Subtraction\n3. Multiplication\n4. Division\n";
+    cin >> choice;
+    switch (choice) {
+    case 1:
+        cout << "Sum=" << c;
+        break;
+    case 2:
+        cout << "Sub=" << c;
+        break;
+    case 3:
+        cout << "Mul=" << c;
+        break;
+    case 4:
+        if (b != 0) {
+            cout << "Div=" << c;
+        }
+        else {
+            cout << "Error: Division by zero";
+        }
+        break;
+    default:
+        cout << "Invalid choice";
+    }
+    return 0;
+}
