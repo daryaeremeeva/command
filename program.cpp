61b0e5be913f0ad72a2962d67ef5148abd0bc20c
Darya <daryaeremeeva2013@mail.ru>
2023-03-27 21:53:32 +0300

    added functions sum and sub

--- program.cpp
+++ program.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include "funcs.h"
 using namespace std;
 
 int main() {
@@ -8,25 +9,26 @@ int main() {
     cout << "Enter your choice:\n1. Addition\n2. Subtraction\n3. Multiplication\n4. Division\n";
     cin >> choice;
     switch (choice) {
-    case 1:
-        cout << "Sum=" << c;
+    case Sum:
+        result = sum(a, b);
         break;
-    case 2:
-        cout << "Sub=" << c;
+    case Sub:
+        result = sub(a, b);
         break;
-    case 3:
+    case Mul:
         cout << "Mul=" << c;
         break;
-    case 4:
-        if (b != 0) {
-            cout << "Div=" << c;
-        }
-        else {
-            cout << "Error: Division by zero";
-        }
+    case Div:
+        cout << "Div=" << c;
         break;
     default:
-        cout << "Invalid choice";
+        cout << "Error: wrong number of function\n";
+        return 1;
     }
+
+    // Вывод результата
+    cout << "Результат: " << result << endl;
+
     return 0;
 }
+
